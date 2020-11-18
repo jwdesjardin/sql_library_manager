f835858aa7c3ead709e87e31edd177b10cd115c0
jwdesjardin <63127698+jwdesjardin@users.noreply.github.com>
2020-11-18 11:03:44 -0800

    changed semantic template names

--- myapp/app.js
+++ myapp/app.js
@@ -66,7 +66,7 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error', { title: 'Server Error' });
+    res.render('server-error', { title: 'Server Error' });
   }
   
 });
